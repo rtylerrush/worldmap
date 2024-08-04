32c79975532728657275b2b39c54d4e4dc198cbf
rtylerrush <roberttylerrush@gmail.com>
2024-08-03 20:01:01 -0400

    Conifugred root component to redirect to /map URL

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WorldComponent} from "./world/world.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/map', pathMatch:'full'},
+  {path: 'map', component: WorldComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
